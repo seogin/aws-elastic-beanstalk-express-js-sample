bea7cb6a5ad6a5238e431f4d9774c01b7012c125
seogin <hsihsj94@gmail.com>
2025-10-30 13:35:51 -0700

    Update greeting message to include student ID

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Seogin Hong'));
+app.get('/', (req, res) => res.send('Hello World! Seogin Hong A01353628'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
